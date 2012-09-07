bf14fb754a969528615d81820f6f3a40c1e32c4d
Jose F. Romaniello <jfromaniello@gmail.com>
2012-09-07 14:36:29 -0300

    fix url for projects

--- javascripts/opensourceprojects.js
+++ javascripts/opensourceprojects.js
@@ -9,7 +9,7 @@ $(function(){
         var repo = repositories[i];
         // if(repo.watchers_count < 2 && repo.fork) continue;
         var rel = repo.watchers_count * (repo.fork ? repo.forks_count : 1);
-        $("<a href='" + repo.url + "' title='" + repo.description + "' rel='" + rel + "'>" + repo.name + "</a><span> </span>")
+        $("<a href='" + repo.html_url + "' title='" + repo.description + "' rel='" + rel + "'>" + repo.name + "</a><span> </span>")
             .appendTo(target);
       }
       // $("#oss a").tagcloud();
