8bdc50343741389fee2c930bc2b3b65c0d0af2bf
Jose F. Romaniello <jfromaniello@gmail.com>
2012-09-07 13:07:17 -0300

    change the style

---
+++ javascripts/opensourceprojects.js
@@ -0,0 +1,19 @@
+$.fn.tagcloud.defaults = {
+  size: {start: 14, end: 22, unit: 'pt'},
+  color: {start: '#cde', end: '#f52'}
+};
+
+$(function(){
+  $.get("https://api.github.com/users/jfromaniello/repos")
+    .done(function(repositories){
+      var target = $("#oss");
+      for(var i in repositories){
+        var repo = repositories[i];
+        // if(repo.watchers_count < 2 && repo.fork) continue;
+        var rel = repo.watchers_count * (repo.fork ? repo.forks_count : 1);
+        $("<a href='" + repo.url + "' title='" + repo.description + "' rel='" + rel + "'>" + repo.name + "</a><span> </span>")
+            .appendTo(target);
+      }
+      // $("#oss a").tagcloud();
+    });
+});
